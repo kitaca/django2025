57ab5e77fa95c4b1b0c2c6b452526a253488584d
kitaca <lij9431@gmail.com>
2025-02-02 17:44:05 +0900

    3주차 django 개발 완료

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,8 +2,18 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class SignupForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': '이메일 입력'})
+    )
+
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", "email")
+        fields = ("username", "email", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['class'] = 'form-control'
